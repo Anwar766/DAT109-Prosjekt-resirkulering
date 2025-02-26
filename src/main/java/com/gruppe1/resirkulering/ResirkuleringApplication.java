18b99e7ee3b9a9de7de7a67a7e094011948301f6
h679178 <679178@stud.hvl.no>
2025-02-26 17:13:23 +0100

    Initial commit

---
+++ src/main/java/com/gruppe1/resirkulering/ResirkuleringApplication.java
@@ -0,0 +1,13 @@
+package com.gruppe1.resirkulering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResirkuleringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResirkuleringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruppe1/resirkulering/ResirkuleringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruppe1.resirkulering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResirkuleringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
